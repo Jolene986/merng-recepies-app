f405f9901afb1d5d4de813ba7ea0c5ca2eca443a
Jolene986 <jojovanovic86@gmail.com>
2021-01-28 13:44:40 +0100

    Apollo client conected

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,41 @@
-import "./App.css";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink,
+  from,
+} from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
 
+import "./App.css";
+import HomePage from "./pages/HomePage";
+
+const errorLink = onError(({ graphqlErrors, networkError }) => {
+  if (graphqlErrors) {
+    graphqlErrors.map(({ message, location, path }) => {
+      console.log(
+        `Graphql error ${message} location: ${location} path: ${path}`
+      );
+      return "error";
+    });
+  }
+  if (networkError) {
+    console.log("networkError", networkError);
+  }
+});
+
+const link = from([errorLink, new HttpLink({ uri: "http://localhost:5000" })]);
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: link,
+});
 function App() {
-  return <div className="App">Heloo</div>;
+  return (
+    <ApolloProvider client={client}>
+      <HomePage />
+    </ApolloProvider>
+  );
 }
 
 export default App;
---
+++ client/src/pages/HomePage.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { useQuery } from "@apollo/client";
+
+import { GET_POSTS } from "../graphQL/Queries";
+
+const HomePage = () => {
+  const { loading, error, data } = useQuery(GET_POSTS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+  console.log(data);
+
+  return (
+    <div>
+      {data.getPosts.map(({ title, id }) => (
+        <h1 key={id}>{title}</h1>
+      ))}
+    </div>
+  );
+};
+
+export default HomePage;
